13e4d0cbc48d3bce121fc0753435f8c62d3a19e3
Grishj <159639347+Grishj@users.noreply.github.com>
2024-02-19 23:58:11 +0545

    Update fulladder.vhdl

--- fulladder/fulladder.vhdl
+++ fulladder/fulladder.vhdl
@@ -60,6 +60,28 @@ begin
 end fa_arch;
 
 		    
+--behavioural architecture
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
 
+entity Full_Adder is
+  port
+  (
+    A    : in std_logic;
+    B    : in std_logic;
+    CIN  : in std_logic;
+    SUM    : out std_logic;
+    CARRY : out std_logic
+  );
+end Full_Adder;
+
+architecture fullAdder of Full_Adder is
+
+  begin
+  SUM<=(A xor B)xor CIN;
+  CARRY<=(A and B)or(CIN and (A xor B));
+  end fullAdder;
+
+		
 
 
